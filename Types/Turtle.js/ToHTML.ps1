74434e29415b2b2ae7555797672ab4ba36aa4a1c
Jake Bolton <ninmonkeys@gmail.com>
2025-11-01 17:48:53 -0500

    feat: `Turtle.Js.ToHTML` uses `Turtle.Arguments` ( re: #341 )

--- Types/Turtle.js/ToHTML.ps1
+++ Types/Turtle.js/ToHTML.ps1
@@ -6,24 +6,30 @@
 .EXAMPLE
     ./TinyTurtle.html.ps1 > ./TinyTurtle.html
 #>
-"<div>"
-"<turtle>"
-"<svg width='100%' height='100%' id='stage'>"
-"<path id='turtle-path' fill='transparent' stroke='currentColor' stroke-width='1%' stroke-linecap='round'></path>"
-"<text id='counter' font-size='12px' x='50%' y='50%' text-anchor='middle' dominant-baseline='middle'></text>"
-"</svg>"
-"</turtle>"
-"<script>"
+
+
+# [string] $commandsToParse = @( '[',
+#    ( $this.ArgumentList.ForEach({ "'{0}'" -f $_ }) -join ', ' ),
+# ' ]' ) -join ' '
+[string] $commandsToParse = $this.ArgumentList.ForEach({ "'{0}'" -f $_ }) -join ', '
 
 @"
+<div><turtle>
+<svg width='100%' height='100%' id='stage'>
+<path id='turtle-path' fill='transparent' stroke='currentColor' stroke-width='1%' stroke-linecap='round'></path>
+<text id='counter' font-size='12px' x='50%' y='50%' text-anchor='middle' dominant-baseline='middle'></text>
+</svg>
+</turtle>
+<script>
 let counter = 0;
 let time = new Date()
 function draw() {
     requestAnimationFrame(draw)
+    const turtle = $this
+    const cmds = [ ${commandsToParse} ]
+    const actions = turtle.parse( ...cmds )
+    actions.forEach( (action) => { turtle.go( action.method, action.arguments ) } )
 
-    let turtle = $this
-
-    turtle.rotate(Math.random() * 360).polygon(100, 8)
     let turtleElement = document.querySelector('turtle')
     let svg = turtleElement.querySelector('#stage')
     svg.setAttribute('viewBox', ``0 0 `${turtle.width} `${turtle.height}``)
@@ -35,6 +41,5 @@ function draw() {
         ```${Math.round(counter/((new Date() - time)/1000)*100)/100} fps``
 }
 draw()
+</script>
 "@
-"</script>"
-"</div>"
