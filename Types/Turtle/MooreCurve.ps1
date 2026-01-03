168a74ead16b06828f39aecb19fb3fccaa61d793
StartAutomating <james.brundage@start-automating.com>
2026-01-02 23:13:17 -0800

    fix: Moore Fixes ( Fixes #370 )

--- Types/Turtle/MooreCurve.ps1
+++ Types/Turtle/MooreCurve.ps1
@@ -6,17 +6,24 @@
 .LINK
     https://en.wikipedia.org/wiki/Moore_curve
 .EXAMPLE
-    $turtle = New-Turtle
-    $turtle.MooreCurve().Pattern.Save("$pwd/MooreCurvePattern.svg")
+    turtle id moore1 moorecurve 42 1
+.EXAMPLE
+    turtle id moore2 moorecurve 42 2
+.EXAMPLE
+    turtle id moore3 moorecurve 42 3
+.EXAMPLE
+    turtle id moore4 moorecurve 42 4
 .EXAMPLE
     Move-Turtle MooreCurve 15 5 |
         Set-Turtle Stroke '#4488ff' |
         Save-Turtle "./MooreCurve.svg"
 #>
 param(
-    [double]$Size = 10,
-    [int]$Order = 4,
-    [double]$Angle = 90
+[double]$Size = $(
+    (Get-Random -Min 42 -Max 84) * (1,-1 |Get-Random)
+),
+[int]$Order = (2,3,4 | Get-Random),
+[double]$Angle = 90
 )        
 
 
@@ -26,9 +33,9 @@ return $this.LSystem(
         L = '-RF+LFL+FR-'
         R = '+LF-RFR-FL+'
     },
-    4, 
+    $Order,
     @{
-        F = { $this.Forward(10) }            
+        F = { $this.Forward($Size) }
         '\+' = { $this.Rotate(90) }            
         '-' = { $this.Rotate(-90) }
     }
