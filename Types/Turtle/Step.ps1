0d15634a19aa36458c48b08d1d8e616ed0bf84b5
James Brundage <+@noreply.github.com>
2026-07-26 11:14:18 -0700

    feat: `Turtle.Step` `Curvature` `CurveType ( Fixes #399, Fixes #408 )
    Adding support for custom curvatures

--- Types/Turtle/Step.ps1
+++ Types/Turtle/Step.ps1
@@ -10,8 +10,53 @@ param(
 # The DeltaX
 [double]$DeltaX = 0, 
 # The DeltaY
-[double]$DeltaY = 0
+[double]$DeltaY = 0,
+
+# The curvature of the step.
+# By default, not a number (no curve).
+[object]$Curvature,
+
+# The type of curve
+[ValidateSet(
+    's', 'SimpleCurve',
+    'q', 'QuadraticCurve',
+    'c', 'CubicCurve',
+    'l', 'LineCurve',
+    't', 'ToCurve'
+)]
+[string]$CurveType = $(
+    if ($this.'CurveType') {
+        $this.'CurveType'
+    } else {
+        's'
+    }
 )
+)
+
+# If no curvature was provided, check the object
+if (
+    ($null -eq $Curvature) -and 
+    ($null -ne $this.'#Curvature')
+) {
+    # and use any value we have set.
+    $Curvature = $this.'#Curvature'
+}
+
+# If we want to draw a curvy turtle
+if ($null -ne ($Curvature -as [double])) {
+    $stepCurve = @(
+        $DeltaX
+        $DeltaY
+        $Curvature
+        if ($CurveType) {
+            $CurveType
+        }
+    )
+    
+    # call step curve and return
+    return $this.StepCurve.Invoke($stepCurve)
+}
+
 # If both coordinates are empty, there is no step
 if ($DeltaX -or $DeltaY) {
     $this.Position = $DeltaX, $DeltaY
