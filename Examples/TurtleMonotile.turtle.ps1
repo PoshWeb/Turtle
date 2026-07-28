a23035ee925e585de4a019b864448c293d5e22d9
James Brundage <+@noreply.github.com>
2026-07-28 14:46:16 -0700

    docs: `TurtleMonotile` example ( Fixes #416 )

---
+++ Examples/TurtleMonotile.turtle.ps1
@@ -0,0 +1,31 @@
+<#
+.SYNOPSIS
+    Turtle Monotile Example
+.DESCRIPTION
+    Draws a Turtle Monotile and generates gradient variations
+#>
+#requires -Module Turtle
+if ($PSScriptRoot) {
+    Push-Location $PSScriptRoot
+}
+
+$monotile = turtle id [TurtleMonotile] rotate -90 TurtleMonotile
+
+$monotile | Save-Turtle .\TurtleMonotile.svg
+
+$monotile.Fill = '#4488ff','#224488', 'linear'
+$monotile.Stroke = '#224488','#4488ff', 'linear'
+
+$monotile | Save-Turtle .\TurtleMonotileGradient.svg
+$monotile | Save-Turtle .\TurtleMonotileGradient.png
+
+$monotile.Fill = '#448822','#228844', 'linear'
+$monotile.Stroke = '#228844','#448822', 'linear'
+
+$monotile | Save-Turtle .\TurtleMonotileGradientGreen.svg
+$monotile | Save-Turtle .\TurtleMonotileGradientGreen.png
+
+if ($PSScriptRoot) {
+    Pop-Location
+}
+
